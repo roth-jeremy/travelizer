785e3537808be4eb79b9e9a4aac6b3a7280aa79e
Dylan Fernandes <dylan.fernandes@heig-vd.ch>
2019-01-17 09:37:14 +0100

    Modif_before1712

--- src/pages/places-visited/places-visited.ts
+++ src/pages/places-visited/places-visited.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AddPlacePage } from '../add-place/add-place';
 import { Nav } from 'ionic-angular';
-
+import { latLng, MapOptions, marker, Marker, tileLayer } from 'leaflet';
 
 /**
  * Generated class for the PlacesVisitedPage page.
@@ -16,14 +16,33 @@ import { Nav } from 'ionic-angular';
   templateUrl: 'places-visited.html',
 })
 export class PlacesVisitedPage {
+  mapOptions: MapOptions;
+  mapMarkers: Marker[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private  nav:Nav) {
+    const tileLayerUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tileLayerOptions = { maxZoom: 18 };
+    this.mapOptions = {
+      layers: [
+        tileLayer(tileLayerUrl, tileLayerOptions)
+      ],
+      zoom: 13,
+      center: latLng(46.778186, 6.641524)
+    };
+    this.mapMarkers = [
+      marker([ 46.778186, 6.641524 ]).bindTooltip("Test Label",{
+        permanent: true, 
+        direction: 'left'
+    }).bindPopup('<b>Hello world!</b><br>I am a popup.<img src="" alt =""/>').openPopup(),
+      marker([ 46.780796, 6.647395 ]),
+      marker([ 46.784992, 6.652267 ])
+    ];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PlacesVisitedPage');
   }
-
+  
   AddPlace() {
     this.nav.push(AddPlacePage);
   }
