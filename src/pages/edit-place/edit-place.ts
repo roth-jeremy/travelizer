b9ed77b17cb377d34acbee2b8506071afdaf1b5d
Dylan Fernandes <dylan.fernandes@heig-vd.ch>
2019-01-25 20:14:25 +0100

    modifJeremAndMe15

--- src/pages/edit-place/edit-place.ts
+++ src/pages/edit-place/edit-place.ts
@@ -4,6 +4,10 @@ import { Place } from '../../models/place';
 import { HttpClient } from '@angular/common/http';
 import { config } from '../../app/config';
 import { Geolocation } from '@ionic-native/geolocation';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { QimgImage } from '../../models/qimg-image';
+import { PictureProvider } from '../../providers/picture/picture';
+import { PlaceRequest, GeoJsonPoint } from '../../models/place-request';
 
 
 
@@ -20,6 +24,9 @@ import { Geolocation } from '@ionic-native/geolocation';
 })
 export class EditPlacePage {
 
+  placeInfo: PlaceRequest;
+  pictureData: string;
+  picture: QimgImage;
   /**
    * The trips API url
    */
@@ -31,9 +38,7 @@ export class EditPlacePage {
    */
   placeUpdateError: boolean;
 
-  placeInfo: Place;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams, private http: HttpClient, private geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: HttpClient, private geolocation: Geolocation, private camera: Camera, private pictureService: PictureProvider) {
     this.placeInfo = this.navParams.get('place');
   }
 
@@ -54,5 +59,38 @@ export class EditPlacePage {
       this.placeUpdateError = true;
       console.warn(`Place update failed: ${err.message}`);
     })
+  }
+  takePicture() {
+    this.pictureService.takeAndUploadPicture().subscribe(picture => {
+      this.picture = picture;
+      this.placeInfo.pictureUrl = this.picture.url;
+      console.log(`Picture url saved ${this.placeInfo.pictureUrl}`);
+    }, err => {
+      console.warn('Could not take picture', err);
+    });
+    /*const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+    this.camera.getPicture(options).then(pictureData => {
+      this.pictureData = pictureData;
+    }).catch(err => {
+      console.warn(`Could not take picture because: ${err.message}`);
+    });*/
+  }
+  addLocation() {
+    const geolocationPromise = this.geolocation.getCurrentPosition();
+    geolocationPromise.then(position => {
+      const coords = position.coords;
+      this.placeInfo.location = new GeoJsonPoint();
+      this.placeInfo.location.type = "Point";
+      this.placeInfo.location.coordinates = [coords.longitude, coords.latitude];
+      console.log(`User is at ${coords.longitude}, ${coords.latitude}`);
+
+    }).catch(err => {
+      console.warn(`Could not retrieve user position because: ${err.message}`);
+    });
   }
 }
